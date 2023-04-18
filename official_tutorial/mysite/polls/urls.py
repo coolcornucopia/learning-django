0db560699fd5d940c3bb0da88d5482fa1c75ecd4
coolcornucopia <coolcornucopia@outlook.com>
2023-04-18 23:18:44 +0200

    official_tutorial: tutorial03: Namespacing URL names (simple comment)
    Signed-off-by: coolcornucopia <coolcornucopia@outlook.com>

--- official_tutorial/mysite/polls/urls.py
+++ official_tutorial/mysite/polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = "polls"
+app_name = "polls" # then use 'polls:details' in html template files
 urlpatterns = [
     # ex: /polls/
     path("", views.IndexView.as_view(), name="index"),
