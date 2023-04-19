f2997aa1ca255e5bbd1b5809d1c4588dbbe90093
coolcornucopia <coolcornucopia@outlook.com>
2023-04-19 18:05:01 +0200

    official_tutorial: tutorial05: Testing the DetailView
    Signed-off-by: coolcornucopia <coolcornucopia@outlook.com>

--- official_tutorial/mysite/polls/views.py
+++ official_tutorial/mysite/polls/views.py
@@ -25,6 +25,12 @@ class DetailView(generic.DetailView):
     #template_name = "polls/detail.html"
     # by default, template_name = "polls/question_detail.html" # <app name>/<model name>_detail.html
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
