1ca5ae3aa0bc0237f6d9158fc6eaa45fb2cecf75
coolcornucopia <coolcornucopia@outlook.com>
2023-04-20 13:15:19 +0200

    official_tutorial: tutorial07: Adding related objects (bis)
    Signed-off-by: coolcornucopia <coolcornucopia@outlook.com>

--- official_tutorial/mysite/polls/admin.py
+++ official_tutorial/mysite/polls/admin.py
@@ -3,14 +3,20 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     # Re-order the fields (ie. by-pass the default order)
     # Split the form up into fieldsets
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
+
 
 # Let admin constructing a default form representation of our Question model
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
