d7344912778ed2dd41428e3c268d16a6e41e5b4d
coolcornucopia <coolcornucopia@outlook.com>
2023-04-20 12:09:18 +0200

    official_tutorial: tutorial07: Customize the admin form
    Signed-off-by: coolcornucopia <coolcornucopia@outlook.com>

--- official_tutorial/mysite/polls/admin.py
+++ official_tutorial/mysite/polls/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    # Re-order the fields (ie. by-pass the default order)
+    fields = ["pub_date", "question_text"]
+
+# Let admin constructing a default form representation of our Question model
+admin.site.register(Question, QuestionAdmin)
