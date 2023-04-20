880762797a423b61320092b2754dd3a8dd143c8f
coolcornucopia <coolcornucopia@outlook.com>
2023-04-20 12:20:52 +0200

    official_tutorial: tutorial07: Customize the admin form (fieldsets)
    Signed-off-by: coolcornucopia <coolcornucopia@outlook.com>

--- official_tutorial/mysite/polls/admin.py
+++ official_tutorial/mysite/polls/admin.py
@@ -5,7 +5,11 @@ from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
     # Re-order the fields (ie. by-pass the default order)
-    fields = ["pub_date", "question_text"]
+    # Split the form up into fieldsets
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 # Let admin constructing a default form representation of our Question model
 admin.site.register(Question, QuestionAdmin)
