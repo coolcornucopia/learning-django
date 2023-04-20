fb084a9ce0f5520162a1fbcbc7144ffbfc147c2e
coolcornucopia <coolcornucopia@outlook.com>
2023-04-20 13:22:58 +0200

    official_tutorial: tutorial07: Customize the admin change list
    Signed-off-by: coolcornucopia <coolcornucopia@outlook.com>

--- official_tutorial/mysite/polls/admin.py
+++ official_tutorial/mysite/polls/admin.py
@@ -16,6 +16,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
     inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 
 # Let admin constructing a default form representation of our Question model
